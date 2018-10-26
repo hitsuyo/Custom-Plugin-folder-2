cca53c8dab8209c8905f4047961de979acc774cd
hitsuyo <nguyentansang3417@gmail.com>
2018-10-26 12:22:12 +0700

    Mytheme Use Shortcode Widget Plugin

---
+++ mytheme-use-shortcode-widget-plugin/includes/muswp-functions.php
@@ -0,0 +1,20 @@
+<?php
+
+// Enable PHP in widgets
+add_filter('widget_text','allow_use_shortcode_or_php_in_widget',100);
+
+function allow_use_shortcode_or_php_in_widget($html)
+{
+	 if(strpos($html,"<"."?php")!==false){
+          ob_start();
+          eval("?".">".$html);
+          $html=ob_get_contents();
+          ob_end_clean();
+     }
+     return $html;
+}
+add_action( 'init', 'myPost_custom_init' ); // important to activate plugin
+
+
+// Enable shortcodes in widgets
+add_filter('widget_text', 'do_shortcode');
---
+++ mytheme-use-shortcode-widget-plugin/mytheme-usw-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Plugin Name: Mytheme Use Shortcode Widget Plugin
+* Plugin URI: 
+* Description: A custom plugin to allow using shortcode or php in widget - muswp - Created on 26/10/2018.
+* Version: 1.0
+* Author: Sang
+* Author URI: 
+* License: 
+**/
+
+
+// Include mdp-functions.php, use require_once to stop the script if mdp-functions.php is not found
+require_once plugin_dir_path(__FILE__) . 'includes/muswp-functions.php';
